f0405fb23f02a5bc16fe549cc8efdfd1fc0638d9
skouriskaze <skouriskaze@gmail.com>
2016-12-19 21:49:31 -0500

    Setting up P5

--- bulletHell.js
+++ bulletHell.js
@@ -1,12 +1,8 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-console.log("We tried");
-<<<<<<< HEAD
+function setup() {
+    createCanvas(1080, 720);
+    background(150);
+}
 
-canvas.width = 1080;
-canvas.height = 720;
+function draw() {
 
-ctx.fillStyle = "rgb(150, 150, 150)";
-=======
->>>>>>> c5ce1141a6a4c432f30bc9acaa3ac6ec1bdbfa3d
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
