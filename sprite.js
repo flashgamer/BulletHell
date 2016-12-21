ab870d0c580a243947d771e41692d5f998b98b25
skouriskaze <skouriskaze@gmail.com>
2016-12-21 17:43:07 -0500

    Bullets LOL

--- sprite.js
+++ sprite.js
@@ -1,17 +1,34 @@
-function Sprite(params, update, draw) {
+function Sprite(params, methods) {
     this.x = params.x || 0;
     this.y = params.y || 0;
     this.src = params.src || 0;
+    this.width = 0;
+    this.height = 0;
 
-    this.img = loadImage(this.src);
-    this.width = params.width || img.width;
-    this.height = params.height || img.height;
 
-    this.update = update || function () {
+    this.setDim = function (img) {
+        console.log(this);
+        this.width = img.width;
+        this.height = img.height;
+        console.log(this.width);
+    }
+
+
+    this.img = res[params.src];
+
+    this.width = this.img.width;
+    this.height = this.img.height;
+
+    if (methods.constructor) {
+        this.constructor = methods.constructor;
+        this.constructor(params);
+    }
+
+    this.update = methods.update || function () {
         // Default update behavior
     }
 
-    this.draw = draw || function () {
+    this.draw = methods.draw || function () {
         // Default draw behavior
         image(this.img, this.x, this.y, this.width, this.height);
     }
