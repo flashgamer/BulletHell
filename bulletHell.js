aacf5b013cd7205798d8f690e7453f34b8843ce5
Jon Jiang <jon.jiang1@gmail.com>
2016-12-21 01:41:46 -0500

    WASD movement supported

--- bulletHell.js
+++ bulletHell.js
@@ -17,19 +17,19 @@ function setup() {
 }
 
 function draw() {
-    background(150);
+    background(100);
     //if (y < canvas.height) {
     //    //rect(x, y, 10, 10);
     //    image(img, x, y);
     //    y += yspeed++ / 2;
     //}
     sprite.draw();
-    if (keyIsDown(LEFT_ARROW))
+    if ((keyIsDown(LEFT_ARROW) || keyIsDown(65)) && sprite.x > 0) // Left or 'A'
         sprite.x -= 3;
-    if (keyIsDown(RIGHT_ARROW))
+    if ((keyIsDown(RIGHT_ARROW) || keyIsDown(68)) && sprite.x < displayWidth) // Right or 'D'
         sprite.x += 3;
-    if (keyIsDown(UP_ARROW))
+    if ((keyIsDown(UP_ARROW) || keyIsDown(87)) && sprite.y > 0) // Up or 'W'
         sprite.y -= 3;
-    if (keyIsDown(DOWN_ARROW))
+    if ((keyIsDown(DOWN_ARROW) || keyIsDown(83)) && sprite.y < displayHeight) // Down or 'S'
         sprite.y += 3;
 }
