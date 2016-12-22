a83a4b800a80cfc614984aec031b670c38236391
Jon Jiang <jon.jiang1@gmail.com>
2016-12-21 22:34:39 -0500

    Added parameters to make spritemapping work

--- sprite.js
+++ sprite.js
@@ -2,6 +2,10 @@ function Sprite(params, update, draw) {
     this.x = params.x || 0;
     this.y = params.y || 0;
     this.src = params.src || 0;
+    this.fps = params.fpt || 0;
+    this.frames = params.frames;
+    this.repeat = params.repeat;
+    this.dirAnim = params.dirAnim || 'horizontal';
 
     this.img = loadImage(this.src);
     this.width = params.width || img.width;
